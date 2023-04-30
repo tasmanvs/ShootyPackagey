09aad7c3b5913c5c014a2aa009f0b737dc5cbfd2
Tasman Smith <tasmanvs@gmail.com>
2023-04-30 10:12:24 -0700

    House spawns coins when destroyed

--- Assets/InifiniteRunner/Runtime/HousePieces.cs
+++ Assets/InifiniteRunner/Runtime/HousePieces.cs
@@ -18,6 +18,6 @@ public class HousePieces : MonoBehaviour
 
     void OnCollisionEnter(Collision collider)
     {
-        transform.parent.GetComponent<House>().DetectCollision(collider);
+        // transform.parent.GetComponent<House>().DetectCollision(collider);
     }
 }
