1f9e76a2949014f42d5d0c303f496f3f7731fc07
Tasman Smith <tasmanvs@gmail.com>
2023-04-30 10:12:24 -0700

    House spawns coins when destroyed

--- Assets/InifiniteRunner/Runtime/House.cs
+++ Assets/InifiniteRunner/Runtime/House.cs
@@ -12,28 +12,56 @@ public class House : MonoBehaviour
 
     [SerializeField]
     GameObject SmallPackage;
-    int IsActive;
+
+    [SerializeField]
+    List<Transform> housePieces;
+
+    [SerializeField]
+    float distanceThreshold = 1f;
+
+    private Dictionary<Transform, Vector3> initialPositions;
+    private bool coinsSpawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        IsActive = Random.Range(0, 2);
         Force = 5.0f;
+        initialPositions = new Dictionary<Transform, Vector3>();
 
+        foreach (Transform piece in housePieces)
+        {
+            initialPositions[piece] = piece.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!coinsSpawned && AllPiecesMoved())
+        {
+            SpawnCoins();
+            coinsSpawned = true;
+        }
     }
 
-    public void DetectCollision(Collision collider)
+    private bool AllPiecesMoved()
     {
-        if(collider.gameObject.name == "SmallPackage")
+        foreach (Transform piece in housePieces)
+        {
+            if (Vector3.Distance(initialPositions[piece], piece.position) < distanceThreshold)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void SpawnCoins()
+    {
+        for (int i = 0; i < 5; i++)
         {
             Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + 5.0f, transform.position.z);
             Instantiate(Coin, spawnPosition, Quaternion.identity).GetComponent<Rigidbody>().AddForce(new Vector2(Random.Range(-0.5f, 0.5f), 1) * Force);
         }
-
     }
 }
