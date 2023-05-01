253a7fa17ff6c6a91049953da1de7ba0280a7d96
Casey Carlin <casey@wholehog-games.com>
2023-04-30 20:11:15 -0700

    randomize box textures

--- Assets/InifiniteRunner/Runtime/Package.cs
+++ Assets/InifiniteRunner/Runtime/Package.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Package : MonoBehaviour
 {
+    public Material[] random_materials;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        int material_index = Random.Range(0, random_materials.Length);
+        GetComponentInChildren<MeshRenderer>().material = random_materials[material_index];
     }
 
     // Update is called once per frame
