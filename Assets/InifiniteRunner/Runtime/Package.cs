51ac554ce3d417e689443a158be7b605efc18402
Casey Carlin <casey@wholehog-games.com>
2023-04-30 20:21:17 -0700

    randomize houses, use the same script as packages

--- Assets/InifiniteRunner/Runtime/Package.cs
+++ Assets/InifiniteRunner/Runtime/Package.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Package : MonoBehaviour
 {
-    public Material[] random_materials;
-
     // Start is called before the first frame update
     void Start()
     {
-        int material_index = Random.Range(0, random_materials.Length);
 
-        GetComponentInChildren<MeshRenderer>().material = random_materials[material_index];
     }
 
     // Update is called once per frame
