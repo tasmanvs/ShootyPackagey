8c9ac8afb914a1d9700e368165336de997c1c1da
Casey Carlin <casey@wholehog-games.com>
2023-04-30 23:24:20 -0700

    potentially controversial camera change

--- Assets/InifiniteRunner/Runtime/FollowCamera.cs
+++ Assets/InifiniteRunner/Runtime/FollowCamera.cs
@@ -18,8 +18,15 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var mousePos = Input.mousePosition;
+        var mouse_normal_x = (mousePos.x / Screen.width) - 0.5f;
+        var mouse_normal_y = (mousePos.y / Screen.height) - 0.5f;
+
         Vector3 newPosition = Player.position - Player.forward * _offset;
         newPosition.y = transform.position.y;
+        newPosition.x -= mouse_normal_x * 4;
+
+        transform.rotation = (Quaternion.Euler(0f , mouse_normal_x * 45f,  0f));
 
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 10.0f);
     }
